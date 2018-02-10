ffa6dc54dbffcca7ff1d4341c30258c3c430b0c1
CChevalier <cchevalier@users.noreply.github.com>
2018-02-10 11:15:16 +0100

    Update URL of json data file again!

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -9,7 +9,14 @@ public class Config {
     static {
         URL url = null;
         try {
-            url = new URL("https://go.udacity.com/xyz-reader-json" );
+            // Original location for json file on dropbox no longer valid
+            // see: https://stackoverflow.com/questions/44918775/xyz-reader-app-sample-not-working
+
+            // Official URL now
+            // url = new URL("https://go.udacity.com/xyz-reader-json" );
+
+            // Modified json file hosted on a private gist
+            url = new URL("https://gist.githubusercontent.com/electron0zero/07a1cddde7948d72dc2c0a12163f92fa/raw/0df255624a2690b6a88dfa13586554ac717a37fa/data.json" );
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
         }
